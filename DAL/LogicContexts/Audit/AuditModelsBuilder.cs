f4f74046d33d7df88b05d70730e1f4d198dd71bb
Neomaster <the@neomaster.net>
2023-02-12 14:39:50 +0300

    [DAL] AuditModelsBuilder

---
+++ DAL/LogicContexts/Audit/AuditModelsBuilder.cs
@@ -0,0 +1,10 @@
+﻿using DAL.LogicContexts.Audit.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.LogicContexts.Audit;
+
+internal static class AuditModelsBuilder
+{
+    internal static ModelBuilder BuildUserModels(this ModelBuilder modelBuilder) => modelBuilder
+        .ApplyConfiguration(new AuditEventConfiguration());
+}
