f492e74251687e946b7e4194cf34809f79198630
Neomaster <the@neomaster.net>
2023-02-12 12:31:54 +0300

    [GraphQLTransport] Configurator

--- GraphQLTransport/GraphQLTransportServicesConfigurator.cs
+++ GraphQLTransport/GraphQLTransportServicesConfigurator.cs
@@ -1,10 +1,19 @@
 ﻿using DAL;
+using GraphQLTransport.Schema;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace GraphQLTransport;
 
 public static class GraphQLTransportServicesConfigurator
 {
-    public static IServiceCollection AddGraphQLTransportServices(this IServiceCollection services, string connectionString) => services
-        .AddDALServices(connectionString);
+    public static IServiceCollection AddGraphQLTransportServices(this IServiceCollection services, string connectionString)
+    {
+        services.AddDALServices(connectionString);
+
+        services
+            .AddGraphQLServer()
+            .AddQueryType<Query>();
+
+        return services;
+    }
 }
