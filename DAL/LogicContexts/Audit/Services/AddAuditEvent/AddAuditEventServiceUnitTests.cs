e87c6af8be6a2bd8908a3f402f0107919978952e
Neomaster <the@neomaster.net>
2023-02-12 15:45:10 +0300

    [DAL] AddAuditEventServiceUnitTests

---
+++ DAL/LogicContexts/Audit/Services/AddAuditEvent/AddAuditEventServiceUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using Autofac.Extras.Moq;
+using Xunit;
+
+namespace DAL.LogicContexts.Audit.Services.AddAuditEvent;
+
+public class AddAuditEventServiceUnitTests : IClassFixture<AppDbContextFixture>
+{
+    private readonly AppDbContext _context;
+    private readonly AutoMock _mock;
+
+    public AddAuditEventServiceUnitTests(AppDbContextFixture fixture)
+    {
+        _context = fixture.Context;
+        _mock = AutoMock.GetLoose(builder =>
+        {
+            builder.RegisterInstance(_context);
+            builder.RegisterType<AddAuditEventService>().As<IAddAuditEventService>();
+        });
+    }
+}
