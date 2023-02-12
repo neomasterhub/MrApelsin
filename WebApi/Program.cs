b1370724ba79a2fd7b3c6286409eef4038bc9e8e
Neomaster <the@neomaster.net>
2023-02-12 12:33:13 +0300

    [WebApi] Swagger has been removed, app.MapGraphQL() has been added.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -9,14 +9,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddGraphQLTransportServices(connectionString);
 
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+app.MapGraphQL();
 app.Run();
