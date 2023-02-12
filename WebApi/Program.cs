cd05a7a69e966c332d230888de176992f9711a68
Neomaster <the@neomaster.net>
2023-02-12 20:34:51 +0300

    [WebApi] Cors policy

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,15 +1,19 @@
 using GraphQLTransport;
+using WebApi.Configurators;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("MSSQL");
 
 builder.Services.AddControllers();
+builder.Services.AddCorsPolicy();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddGraphQLTransportServices(connectionString);
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseRouting();
+app.UseCors(CorsPolicyConfigurator.CorsPolicyName);
 app.UseAuthorization();
 app.MapControllers();
 app.MapGraphQL();
