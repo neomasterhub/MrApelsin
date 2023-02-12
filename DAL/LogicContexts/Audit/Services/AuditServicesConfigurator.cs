53536aac961a8514d73625fb5617ccdf6ee6bf04
Neomaster <the@neomaster.net>
2023-02-12 14:52:57 +0300

    [DAL] AuditServicesConfigurator

---
+++ DAL/LogicContexts/Audit/Services/AuditServicesConfigurator.cs
@@ -0,0 +1,10 @@
+﻿using DAL.LogicContexts.Audit.Services.AddAuditEvent;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DAL.LogicContexts.Audit.Services;
+
+internal static class AuditServicesConfigurator
+{
+    public static IServiceCollection AddAuditServices(this IServiceCollection services) => services
+        .AddScoped<IAddAuditEventService, AddAuditEventService>();
+}
