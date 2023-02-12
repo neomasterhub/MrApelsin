276d5cf5001057659bb91b6d8b469c28cd807b63
Neomaster <the@neomaster.net>
2023-02-12 22:07:25 +0300

    [Client] Apollo test

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
+  sub?: Subscription;
+
+  constructor(private apollo: Apollo) {
+  }
+
+  getAuditEvents() {
+    this.sub = this.apollo
+    .query({
+      query: gql`query {
+        auditEvents {
+          id
+          url
+        }
+      }`
+    })
+    .subscribe({
+      next: (result) => {
+        console.log(result.data);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+      complete: () => {
+        console.log({
+          'sub closed': this.sub?.closed,
+        }); // true
+      }
+    });
+  }
 }
