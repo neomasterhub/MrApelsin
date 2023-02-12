06490f53929243b944b210c1bc01f0dbaf634740
Neomaster <the@neomaster.net>
2023-02-12 14:22:06 +0300

    [DataModel] AuditEvent's Object, Subject have been added.

--- DataModel/AuditEvent.cs
+++ DataModel/AuditEvent.cs
@@ -4,5 +4,7 @@ public class AuditEvent
 {
     public int Id { get; set; }
     public DateTime DateTimeUtc { get; set; }
+    public string Object { get; set; }
+    public string Subject { get; set; }
     public string Description { get; set; }
 }
