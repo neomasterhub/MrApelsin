0abad0b5171f934d5b1e231547f748ae39b88c06
Neomaster <the@neomaster.net>
2023-02-12 14:39:22 +0300

    [DAL] IAuditContext

---
+++ DAL/LogicContexts/Audit/IAuditContext.cs
@@ -0,0 +1,9 @@
+﻿using DataModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.LogicContexts.Audit;
+
+internal interface IAuditContext
+{
+    public DbSet<AuditEvent> AuditEvents { get; set; }
+}
