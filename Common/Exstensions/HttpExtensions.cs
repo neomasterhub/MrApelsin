e49ce3dec096fa98099461e14ad5b0ece26d46a9
Neomaster <the@neomaster.net>
2023-02-12 15:21:05 +0300

    [Common] HttpExtensions.GetUrl()

---
+++ Common/Exstensions/HttpExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Common.Exstensions;
+
+public static class HttpExtensions
+{
+    public static string GetUrl(this HttpRequest request)
+    {
+        var url = string.Concat(
+            request.Scheme,
+            "://",
+            request.Host,
+            request.Path,
+            request.QueryString.Value);
+
+        return url;
+    }
+}
