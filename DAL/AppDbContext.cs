afa20df0840f417ac13a5e94501d33d80c4cf8c0
Neomaster <the@neomaster.net>
2023-02-12 11:59:16 +0300

    AppDbContext

---
+++ DAL/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DAL.Configurations;
+using DataModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL;
+
+public class AppDbContext : DbContext
+{
+    public static readonly string Schema = "mr.apelsin";
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<AuditEvent> AuditEvents { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder) => builder
+        .ApplyConfiguration(new AuditEventConfiguration())
+        .HasDefaultSchema(Schema);
+}
