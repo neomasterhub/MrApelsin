c755e8dcaf5af6c6710d4eefb7f85200b3311ebe
Neomaster <the@neomaster.net>
2023-02-12 12:01:13 +0300

    DALServicesConfigurator

---
+++ DAL/DALServicesConfigurator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DAL;
+
+public static class DALServicesConfigurator
+{
+    public static IServiceCollection AddDalServices(this IServiceCollection services, string connectionString) => services
+        .AddDbContext<AppDbContext>(options => options
+            .UseSqlServer(connectionString, options => options
+                .MigrationsAssembly("WebApi")
+                .MigrationsHistoryTable(tableName: "__EFMigrationsHistory", schema: AppDbContext.Schema)));
+}
