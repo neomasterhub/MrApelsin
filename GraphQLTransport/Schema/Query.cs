44ef958de8d405a8350e10fe32317d796a14045e
Neomaster <the@neomaster.net>
2023-02-12 12:31:30 +0300

    [GraphQLTransport] Query.GetAuditEvents()

---
+++ GraphQLTransport/Schema/Query.cs
@@ -0,0 +1,9 @@
+﻿using DAL;
+using DataModel;
+
+namespace GraphQLTransport.Schema;
+
+public class Query
+{
+    public IQueryable<AuditEvent> GetAuditEvents(AppDbContext context) => context.AuditEvents;
+}
