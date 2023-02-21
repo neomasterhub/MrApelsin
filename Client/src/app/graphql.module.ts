c6dc8f1dea65e314edeee7108a9497da34558237
Neomaster <the@neomaster.net>
2023-02-21 16:18:59 +0300

    [Client] Apollo is configured to work with subscriptions via a websocket uri.

--- Client/src/app/graphql.module.ts
+++ Client/src/app/graphql.module.ts
@@ -1,13 +1,35 @@
 import { NgModule } from '@angular/core';
-import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import { ApolloClientOptions, InMemoryCache, split } from '@apollo/client/core';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { getMainDefinition } from '@apollo/client/utilities';
 import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
-
-const uri = 'http://localhost:5008/graphql';
+import { createClient } from 'graphql-ws';
+import { environment } from '../environments/environment';
 
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+  const http = httpLink.create({
+    uri: environment.graphqlOrigins.http,
+  });
+
+  const ws = new GraphQLWsLink(createClient({
+    url: environment.graphqlOrigins.ws,
+  }));
+
+  const splitLink = split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+        definition.kind === 'OperationDefinition'
+        && definition.operation === 'subscription'
+      );
+    },
+    ws,
+    http,
+  );
+
   return {
-    link: httpLink.create({ uri }),
+    link: splitLink,
     cache: new InMemoryCache(),
   };
 }
