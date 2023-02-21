0d5cc2094221a89d361f92f7f65b6e220e7d4ffb
Neomaster <the@neomaster.net>
2023-02-21 14:54:40 +0300

    [GraphQLTransport] GetAppVersion() sends a message to the server in the subscription.

--- GraphQLTransport/Schema/Query.cs
+++ GraphQLTransport/Schema/Query.cs
@@ -1,9 +1,25 @@
-﻿using DAL;
+﻿using Common.Versioning;
+using DAL;
 using DataModel;
+using HotChocolate.Subscriptions;
 
 namespace GraphQLTransport.Schema;
 
 public class Query
 {
     public IQueryable<AuditEvent> GetAuditEvents(AppDbContext context) => context.AuditEvents;
+
+    public async Task<AppVersion> GetAppVersion([Service] ITopicEventSender sender)
+    {
+        var result = AppInfo.Info.Instance.AppVersion;
+        var message = new ServerMessage
+        {
+            MessageType = ServerMessageType.AppVersion,
+            Text = result.ToString(),
+        };
+
+        await sender.SendAsync(nameof(Subscription.ServerMessageReceived), message);
+
+        return result;
+    }
 }
