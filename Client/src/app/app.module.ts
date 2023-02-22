887cb33e1120b0fc1df1f34c17c62b17d4931906
Neomaster <the@neomaster.net>
2023-02-22 18:16:37 +0300

    [Client] @ngrx/effects

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,6 +23,7 @@ import { GraphQLModule } from './graphql.module';
     }, {
       metaReducers,
     }),
+    EffectsModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent],
