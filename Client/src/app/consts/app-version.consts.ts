164e82af7684b8fb53c774a4b940d206aa5b1a5b
Neomaster <the@neomaster.net>
2023-02-24 22:09:52 +0300

    [Tools] versioning.tagrets generates JS object const.

--- Client/src/app/consts/app-version.consts.ts
+++ Client/src/app/consts/app-version.consts.ts
@@ -1,2 +1,5 @@
-﻿export const versionNumber = '1.0.0.0';
-export const versionDatetime = '2023.02.21 14:29:04';
+﻿// auto-generated
+export const version = {
+  versionNumber: '1.0.0.0',
+  versionDatetime: '2023.02.24 22:09:04',
+}
