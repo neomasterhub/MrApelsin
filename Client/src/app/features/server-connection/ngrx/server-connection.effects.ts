0351f95cc65bb6bf4674c043d886a5379df328f0
Neomaster <the@neomaster.net>
2023-02-24 21:10:04 +0300

    [Client] appIsCreatedEffect$

--- Client/src/app/features/server-connection/ngrx/server-connection.effects.ts
+++ Client/src/app/features/server-connection/ngrx/server-connection.effects.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@angular/core';
-import { Actions } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { map, tap } from 'rxjs';
+import { isCreated } from '../../../ngrx/app.actions';
+import { ConfigureGraphqlWsService } from '../services/configure-graphql-ws.service';
 import { ConsumeServerMessagesService } from '../services/consume-server-messages.service';
+import { isConfiguring } from './server-connection.actions';
 
 @Injectable()
 export class ServerConnectionEffects {
   constructor(
     private readonly actions$: Actions,
-    private readonly service: ConsumeServerMessagesService,
+    private readonly configureGraphqlWsService: ConfigureGraphqlWsService,
+    private readonly consumeServerMessagesService: ConsumeServerMessagesService,
   ) {
   }
+
+  public appIsCreatedEffect$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(isCreated),
+      tap(() => this.configureGraphqlWsService.configure()),
+      map(() => isConfiguring()),
+    );
+  });
 }
