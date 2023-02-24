c6049cce8916f93bf4827db5ca9eeb63cf3bab83
Neomaster <the@neomaster.net>
2023-02-24 13:35:04 +0300

    [Client] ConsumeServerMessagesService dispatches serverMessageIsReceived().

--- Client/src/app/features/server-connection/services/consume-server-messages.service.ts
+++ Client/src/app/features/server-connection/services/consume-server-messages.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { PingGQL, ServerMessageReceivedGQL } from '../../../../graphql/generated/graphql';
-import { isFailed } from '../ngrx/server-connection.actions';
+import { tap } from 'rxjs';
+import { ServerMessageReceivedGQL } from '../../../../graphql/generated/graphql';
+import { isFailed, serverMessageIsReceived } from '../ngrx/server-connection.actions';
 
 @Injectable()
 export class ConsumeServerMessagesService {
   constructor(
     private readonly serverMessageReceivedGQL: ServerMessageReceivedGQL,
-    private readonly ping: PingGQL,
     private readonly store: Store,
   ) {
   }
 
   subscribe() {
     this.serverMessageReceivedGQL.subscribe()
+      .pipe(
+        tap(({ data }) => this.store.dispatch(serverMessageIsReceived({ serverMessage: data!.serverMessageReceived! }))),
+      )
       .subscribe({
         error: () => this.store.dispatch(isFailed()),
       });
