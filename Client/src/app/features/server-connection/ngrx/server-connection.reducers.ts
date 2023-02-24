d4668a1e938d1d7d8ab19bd3f6429fbabe3c412e
Neomaster <the@neomaster.net>
2023-02-24 12:09:26 +0300

    [Client] ServerStatusReducer has been improved.

--- Client/src/app/features/server-connection/ngrx/server-connection.reducers.ts
+++ Client/src/app/features/server-connection/ngrx/server-connection.reducers.ts
@@ -1,10 +1,8 @@
 import { createReducer, on } from '@ngrx/store';
 import { isEstablished, isEstablishing, isFailed } from './server-connection.actions';
 
-const initialState = 'unknown';
-
 export const ServerStatusReducer = createReducer(
-  initialState,
+  'unknown',
   on(isEstablishing, () => 'ping'),
   on(isEstablished, () => 'online'),
   on(isFailed, () => 'offline'),
