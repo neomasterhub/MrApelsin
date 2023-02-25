2a0d0103072925872be768519aa606757a32e360
Neomaster <the@neomaster.net>
2023-02-25 09:16:46 +0300

    [Tools] Versioning has been improved.

--- Client/src/app/consts/app-version.ts
+++ Client/src/app/consts/app-version.ts
@@ -1,5 +1,5 @@
 ﻿// auto-generated
 export const version = {
   versionNumber: '1.0.0.0',
-  versionDatetime: '2023.02.24 22:09:04',
+  versionDatetime: '2023.02.25 09:16:06',
 }
--- Client/src/app/features/footer/components/footer.component.ts
+++ Client/src/app/features/footer/components/footer.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { version } from '../../../consts/app-version.consts';
+import { version } from '../../../consts/app-version';
 import { ServerStatusModel } from '../../server-connection/models/server-status.model';
 
 @Component({
