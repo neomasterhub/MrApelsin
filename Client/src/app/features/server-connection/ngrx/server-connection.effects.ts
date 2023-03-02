13400dd1207be730423f76a5c809a6ac52478906
Neomaster <the@neomaster.net>
2023-03-02 11:36:18 +0300

    [Client] isCreated -> typeIsDefined only

--- Client/src/app/features/server-connection/ngrx/server-connection.effects.ts
+++ Client/src/app/features/server-connection/ngrx/server-connection.effects.ts
@@ -1,49 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { delay, map, tap } from 'rxjs';
+import { map } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 import { isCreated } from '../../../ngrx/app.actions';
-import { ConfigureGraphqlWsService } from '../services/configure-graphql-ws.service';
-import { ConsumeServerMessagesService } from '../services/consume-server-messages.service';
-import { isConfigured, isEstablishing, isFailed, isWaiting } from './server-connection.actions';
+import { typeIsDefined } from './server-connection.actions';
 
 @Injectable()
 export class ServerConnectionEffects {
-  constructor(
-    private readonly actions$: Actions,
-    private readonly configureGraphqlWsService: ConfigureGraphqlWsService,
-    private readonly consumeServerMessagesService: ConsumeServerMessagesService,
-  ) {
+  constructor(private readonly actions$: Actions) {
   }
 
   public appIsCreatedEffect$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(isCreated),
-      map(() => this.configureGraphqlWsService.configure()),
-    );
-  });
-
-  public serverConnectionIsConfiguredEffect$ = createEffect(() => {
-    return this.actions$.pipe(
-      ofType(isConfigured),
-      tap(() => this.consumeServerMessagesService.subscribe()),
-      map(() => isEstablishing()),
-    );
-  });
-
-  public serverConnectionIsFailedEffect$ = createEffect(() => {
-    return this.actions$.pipe(
-      ofType(isFailed),
-      map(() => isWaiting()),
-    );
-  });
-
-  public serverConnectionIsWaitingEffect$ = createEffect(() => {
-    return this.actions$.pipe(
-      ofType(isWaiting),
-      delay(environment.serverConnectionRetryTimeoutSeconds * 1000),
-      tap(() => this.consumeServerMessagesService.subscribe()),
-      map(() => isEstablishing()),
+      map(() => typeIsDefined({ connectionType: environment.serverConnectionType })),
     );
   });
 }
