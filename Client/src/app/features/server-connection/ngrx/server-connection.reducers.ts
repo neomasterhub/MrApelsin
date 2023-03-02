2e5f9064eb1ef5587a9927725b77422159e0f6f0
Neomaster <the@neomaster.net>
2023-03-02 11:51:33 +0300

    [Client] ServerMessageReducer has been returned to server-connection.reducers/.

--- Client/src/app/features/server-connection/ngrx/server-connection.reducers.ts
+++ Client/src/app/features/server-connection/ngrx/server-connection.reducers.ts
@@ -1,10 +1,23 @@
 import { createReducer, on } from '@ngrx/store';
 import { ServerStatus } from '../activity-types/server-status';
-import { isEstablished, isEstablishing, isFailed } from './server-connection.actions';
+import { IServerMessageModel } from '../models/server-message.model';
+import { isEstablished, isEstablishing, isFailed, serverMessageIsReceived } from './server-connection.actions';
 
 export const ServerStatusReducer = createReducer(
   ServerStatus.unknown,
   on(isEstablishing, () => ServerStatus.ping),
   on(isEstablished, () => ServerStatus.online),
   on(isFailed, () => ServerStatus.offline),
+);
+
+export const ServerMessageReducer = createReducer(
+  undefined as IServerMessageModel | undefined,
+  on(serverMessageIsReceived, (state, { serverMessage }) => ({
+    datetime: new Date(),
+    payload: {
+      messageType: serverMessage.messageType,
+      contentType: serverMessage.contentType,
+      content: serverMessage.content,
+    },
+  })),
 );
