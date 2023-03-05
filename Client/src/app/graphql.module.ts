67d527eb6043f24a528f9fcd5b8cd84463eb5360
Neomaster <the@neomaster.net>
2023-03-05 20:24:15 +0300

    [Client] Graphql module uses RetryLink.

--- Client/src/app/graphql.module.ts
+++ Client/src/app/graphql.module.ts
@@ -1,31 +1,34 @@
 import { NgModule } from '@angular/core';
-import { ApolloClientOptions, InMemoryCache, split } from '@apollo/client/core';
+import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import { RetryLink } from '@apollo/client/link/retry';
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { getMainDefinition } from '@apollo/client/utilities';
 import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { environment } from '../environments/environment';
 import { graphqlWsClient } from './consts/graphql-ws-client';
+import { retryLinkOptions } from './consts/retry-link-options';
 
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
-  const http = httpLink.create({
+  const splitLinkHttp = httpLink.create({
     uri: environment.graphqlOrigins.http,
   });
 
-  const ws = new GraphQLWsLink(graphqlWsClient);
+  const splitLinkWs = new GraphQLWsLink(graphqlWsClient);
 
-  const splitLink = split(
-    ({ query }) => {
-      const definition = getMainDefinition(query);
+  const splitLink = new RetryLink(retryLinkOptions)
+    .split(
+      ({ query }) => {
+        const definition = getMainDefinition(query);
 
-      return (
-        definition.kind === 'OperationDefinition'
-        && definition.operation === 'subscription'
-      );
-    },
-    ws,
-    http,
-  );
+        return (
+          definition.kind === 'OperationDefinition'
+          && definition.operation === 'subscription'
+        );
+      },
+      splitLinkWs,
+      splitLinkHttp,
+    );
 
   return {
     link: splitLink,
