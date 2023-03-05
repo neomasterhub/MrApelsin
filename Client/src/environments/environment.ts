59b76f3224edcd8cb9e7da750b1e14219fbb5fb8
Neomaster <the@neomaster.net>
2023-03-06 00:01:31 +0300

    [Client] longPollingOperations has been deleted from environment.

--- Client/src/environments/environment.ts
+++ Client/src/environments/environment.ts
@@ -1,16 +1,4 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-import { ILongPollingOperation } from '../app/activity-types/long-polling-operation';
 import { ServerConnectionType } from '../app/features/server-connection/activity-types/server-connection-type';
-
-const longPollingOperations: ILongPollingOperation[] = [
-  {
-    name: 'GetHttpPong',
-    delay: 5000,
-  },
-];
 
 export const environment = {
   production: false,
@@ -22,16 +10,7 @@ export const environment = {
     connectionType: ServerConnectionType.longPolling,
     attemptIntervalSeconds: 10,
     longPolling: {
-      operations: longPollingOperations,
+      operationName: 'GetHttpPong',
     },
   },
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
