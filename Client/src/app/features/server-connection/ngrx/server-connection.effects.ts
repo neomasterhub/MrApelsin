1153eefb4306d8c3499f8851e5def043c67aecca
Neomaster <the@neomaster.net>
2023-03-05 23:14:59 +0300

    [Client] serverConnectionIsFailedEffect$ in ServerConnectionEffects.

--- Client/src/app/features/server-connection/ngrx/server-connection.effects.ts
+++ Client/src/app/features/server-connection/ngrx/server-connection.effects.ts
@@ -3,7 +3,7 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { map } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 import { isCreated } from '../../../ngrx/app.actions';
-import { typeIsDefined } from './server-connection.actions';
+import { isFailed, isWaiting, typeIsDefined } from './server-connection.actions';
 
 @Injectable()
 export class ServerConnectionEffects {
@@ -14,6 +14,13 @@ export class ServerConnectionEffects {
     return this.actions$.pipe(
       ofType(isCreated),
       map(() => typeIsDefined({ connectionType: environment.serverConnection.connectionType })),
+    );
+  });
+
+  public serverConnectionIsFailedEffect$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(isFailed),
+      map(() => isWaiting()),
     );
   });
 }
