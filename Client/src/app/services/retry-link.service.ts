d75052eafdd5ec25a6b203563cff31192fda2a8b
Neomaster <the@neomaster.net>
2023-03-06 00:05:20 +0300

    [Client] RetryLinkService has been updated.

--- Client/src/app/services/retry-link.service.ts
+++ Client/src/app/services/retry-link.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@angular/core';
 import { RetryLink } from '@apollo/client/link/retry';
-import { environment } from '../../environments/environment';
+import { longPollingOperations } from '../consts/long-polling-operations';
 
 @Injectable()
 export class RetryLinkService {
   readonly retryLinkOptions: RetryLink.Options;
 
   constructor() {
-    const longPollingOperations = environment.serverConnection.longPolling.operations;
-
     this.retryLinkOptions = {
       attempts: (count, operation) => {
         return longPollingOperations
