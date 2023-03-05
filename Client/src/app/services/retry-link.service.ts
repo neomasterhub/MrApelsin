a880ca834a7a284aff39f21fd6e572f8d2fd5c34
Neomaster <the@neomaster.net>
2023-03-06 00:12:44 +0300

    [Client] RetryLinkService dispatches isFailed().

--- Client/src/app/services/retry-link.service.ts
+++ Client/src/app/services/retry-link.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
 import { RetryLink } from '@apollo/client/link/retry';
+import { Store } from '@ngrx/store';
+import { environment } from '../../environments/environment';
 import { longPollingOperations } from '../consts/long-polling-operations';
+import { isFailed } from '../features/server-connection/ngrx/server-connection.actions';
 
 @Injectable()
 export class RetryLinkService {
   readonly retryLinkOptions: RetryLink.Options;
 
-  constructor() {
+  constructor(private readonly store: Store) {
     this.retryLinkOptions = {
       attempts: (count, operation) => {
+        if (operation.operationName == environment.serverConnection.longPolling.operationName) {
+          this.store.dispatch(isFailed());
+        }
+
         return longPollingOperations
           .some((o) => o.name === operation.operationName);
       },
