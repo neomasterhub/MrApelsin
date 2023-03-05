6d3806aea027513328d5a0a7fb9e4268ac96b98d
Neomaster <the@neomaster.net>
2023-03-06 01:26:33 +0300

    [Client] LongPollingServerConnectionEffects.serverConnectionIsEstablishedEffect$

--- Client/src/app/features/server-connection/long-polling/ngrx/long-polling-server-connection.effects.ts
+++ Client/src/app/features/server-connection/long-polling/ngrx/long-polling-server-connection.effects.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { filter, map, tap } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { filter, map, tap, withLatestFrom } from 'rxjs';
 import { ServerConnectionType } from '../../activity-types/server-connection-type';
-import { isEstablishing, typeIsDefined } from '../../ngrx/server-connection.actions';
+import { isEstablished, isEstablishing, isWaiting, typeIsDefined } from '../../ngrx/server-connection.actions';
 import { GetHttpPongService } from '../services/get-http-pong.service';
 
 @Injectable()
@@ -10,6 +11,7 @@ export class LongPollingServerConnectionEffects {
   constructor(
     private readonly actions$: Actions,
     private readonly getHttpPongService: GetHttpPongService,
+    private readonly store: Store<{ serverConnectionType: ServerConnectionType }>,
   ) {
   }
 
@@ -19,6 +21,15 @@ export class LongPollingServerConnectionEffects {
       filter((a) => a.connectionType === ServerConnectionType.longPolling),
       tap(() => this.getHttpPongService.ping()),
       map(() => isEstablishing()),
+    );
+  });
+
+  public serverConnectionIsEstablishedEffect$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(isEstablished),
+      withLatestFrom(this.store.select(s => s.serverConnectionType)),
+      filter(([, serverConnectionType]) => serverConnectionType == ServerConnectionType.longPolling),
+      map(() => isWaiting()),
     );
   });
 }
