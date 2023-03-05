c1a6d9494ae78e2d68e74450b26572206a7e0df9
Neomaster <the@neomaster.net>
2023-03-05 19:06:18 +0300

    [Client] LongPollingServerConnectionEffects

---
+++ Client/src/app/features/server-connection/long-polling/services/long-polling-server-connection.effects.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { filter, map, tap } from 'rxjs';
+import { ServerConnectionType } from '../../activity-types/server-connection-type';
+import { isEstablishing, typeIsDefined } from '../../ngrx/server-connection.actions';
+import { GetHttpPongService } from './get-http-pong.service';
+
+@Injectable()
+export class LongPollingServerConnectionEffects {
+  constructor(
+    private readonly actions$: Actions,
+    private readonly getHttpPongService: GetHttpPongService,
+  ) {
+  }
+
+  public serverConnectionIsDefinedEffect$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(typeIsDefined),
+      filter((a) => a.connectionType === ServerConnectionType.longPolling),
+      tap(() => this.getHttpPongService.ping()),
+      map(() => isEstablishing()),
+    );
+  });
+}
