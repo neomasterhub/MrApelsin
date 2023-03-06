64681d6fd3d3c998e57d2493bb53924eed6f7434
Neomaster <the@neomaster.net>
2023-03-06 21:50:27 +0300

    [Client] serverConnectionIsWaitingEffect$ has been added to view "ping" status in html.

--- Client/src/app/features/server-connection/long-polling/ngrx/long-polling-server-connection.effects.ts
+++ Client/src/app/features/server-connection/long-polling/ngrx/long-polling-server-connection.effects.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { filter, map, tap, withLatestFrom } from 'rxjs';
+import { delay, filter, map, tap, withLatestFrom } from 'rxjs';
+import { environment } from '../../../../../environments/environment';
 import { ServerConnectionType } from '../../activity-types/server-connection-type';
-import { engineIsStarted, isEstablished, isWaiting, typeIsDefined } from '../../ngrx/server-connection.actions';
+import {
+  engineIsStarted,
+  isEstablished,
+  isEstablishing,
+  isFailed,
+  isWaiting,
+  typeIsDefined,
+} from '../../ngrx/server-connection.actions';
 import { GetHttpPongService } from '../services/get-http-pong.service';
 
 @Injectable()
@@ -30,6 +38,16 @@ export class LongPollingServerConnectionEffects {
       withLatestFrom(this.store.select(s => s.serverConnectionType)),
       filter(([, serverConnectionType]) => serverConnectionType == ServerConnectionType.longPolling),
       map(() => isWaiting()),
+    );
+  });
+
+  public serverConnectionIsWaitingEffect$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(isFailed),
+      withLatestFrom(this.store.select(s => s.serverConnectionType)),
+      filter(([, serverConnectionType]) => serverConnectionType == ServerConnectionType.longPolling),
+      delay(environment.serverConnection.attemptIntervalSeconds * 1000), // for "ping" status in html
+      map(() => isEstablishing()),
     );
   });
 }
