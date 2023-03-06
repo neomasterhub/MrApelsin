6399b9fe9a8d2ec3b6eb769388e4d3442534ff75
Neomaster <the@neomaster.net>
2023-03-06 21:09:25 +0300

    [Client] HttpLinkService

---
+++ Client/src/app/services/http-link.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpLink } from 'apollo-angular/http';
+import { environment } from '../../environments/environment';
+
+@Injectable()
+export class HttpLinkService {
+  readonly httpLink;
+
+  constructor(httpLink: HttpLink) {
+    this.httpLink = httpLink.create({
+      uri: environment.graphqlOrigins.http,
+    });
+  }
+}
