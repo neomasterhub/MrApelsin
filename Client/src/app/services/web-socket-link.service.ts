ed4bca94f1b8495dc1512acb8eedfedeae8e4f96
Neomaster <the@neomaster.net>
2023-03-06 21:09:33 +0300

    [Client] WebSocketLinkService

---
+++ Client/src/app/services/web-socket-link.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { Store } from '@ngrx/store';
+import { createClient } from 'graphql-ws';
+import { environment } from '../../environments/environment';
+import { isEstablished, isFailed } from '../features/server-connection/ngrx/server-connection.actions';
+
+@Injectable()
+export class WebSocketLinkService {
+  readonly webSocketLink;
+
+  constructor(store: Store) {
+    const graphqlWsClient = createClient({
+      url: environment.graphqlOrigins.ws,
+    });
+
+    graphqlWsClient.on('connected', () => store.dispatch(isEstablished()));
+    graphqlWsClient.on('error', () => store.dispatch(isFailed()));
+
+    this.webSocketLink = new GraphQLWsLink(graphqlWsClient);
+  }
+}
