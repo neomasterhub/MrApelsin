c5f89268c9204a0002910bbb7ad9c04a7bf0d4cb
Neomaster <the@neomaster.net>
2023-03-07 04:51:07 +0300

    [Client] GetHttpPongService.intervalPing() dispatches serverMessageIsReceived action.

--- Client/src/app/features/server-connection/long-polling/services/get-http-pong.service.ts
+++ Client/src/app/features/server-connection/long-polling/services/get-http-pong.service.ts
@@ -3,7 +3,7 @@ import { Store } from '@ngrx/store';
 import { tap } from 'rxjs';
 import { environment } from '../../../../../environments/environment';
 import { GetHttpPongGQL } from '../../../../../graphql/generated/graphql';
-import { isEstablished, isEstablishing } from '../../ngrx/server-connection.actions';
+import { isEstablished, isEstablishing, serverMessageIsReceived } from '../../ngrx/server-connection.actions';
 
 @Injectable()
 export class GetHttpPongService {
@@ -24,11 +24,14 @@ export class GetHttpPongService {
   intervalPing() {
     this.valueChanges
       .pipe(
-        tap(({ loading }) => {
+        tap(({ data, loading }) => {
           if (loading) {
             this.store.dispatch(isEstablishing());
           } else {
             this.store.dispatch(isEstablished());
+            this.store.dispatch(serverMessageIsReceived({
+              serverMessage: data!.ping!
+            }));
           }
         }),
       )
