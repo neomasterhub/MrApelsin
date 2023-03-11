641f141b96d5570772207589619c4210167ae89e
Neomaster <the@neomaster.net>
2023-03-11 20:14:05 +0300

    [Client] Http request spinner banner

--- Client/src/app/features/loading-animation/components/loading-animation.component.ts
+++ Client/src/app/features/loading-animation/components/loading-animation.component.ts
@@ -4,6 +4,7 @@ import { SpinnerService } from '../services/spinner.service';
 @Component({
   selector: 'app-loading-animation',
   templateUrl: './loading-animation.component.html',
+  styleUrls: ['./loading-animation.component.less'],
 })
 export class LoadingAnimationComponent {
   constructor(public readonly spinnerService: SpinnerService) {
